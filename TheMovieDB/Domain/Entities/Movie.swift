4de651f5b5b6c9cae91b9d9d395357cfad2f6867
Yaser Bahrami <yaserbahrami@outlook.com>
2025-01-13 02:27:15 +0300

    fix movie entity

--- TheMovieDB/Domain/Entities/Movie.swift
+++ TheMovieDB/Domain/Entities/Movie.swift
@@ -15,7 +15,7 @@ struct Movie: Decodable {
     let id: Int
     let title: String
     let overview: String
-    let posterPath: String
+    let posterPath: String?
     var backdropPath: String?
     var releaseDate: String?
     var voteAverage: Double
@@ -24,7 +24,9 @@ struct Movie: Decodable {
     var originalLanguage: String?
     
     enum CodingKeys: String, CodingKey {
-        case id, title, overview
+        case id
+        case title
+        case overview
         case posterPath = "poster_path"
         case backdropPath = "backdrop_path"
         case releaseDate = "first_air_date"
@@ -32,5 +34,15 @@ struct Movie: Decodable {
         case voteAverage = "vote_average"
         case adult
         case originalLanguage = "original_language"
+    }
+    
+    var posterImageURL: URL? {
+        guard let path = posterPath else { return nil }
+        return URL(string: AppConfig.imageBaseURL + path)
+    }
+    
+    var backdropImageURL: URL? {
+        guard let path = backdropPath else { return nil }
+        return URL(string: AppConfig.imageBaseURL + path)
     }
 }
