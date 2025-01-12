8142b28c1bf57389d56663e73f3d3ecf70778976
Yaser Bahrami <yaserbahrami@outlook.com>
2025-01-13 02:28:31 +0300

    update plist

--- TheMovieDB/App/AppConfig.swift
+++ TheMovieDB/App/AppConfig.swift
@@ -10,14 +10,21 @@ import Foundation
 struct AppConfig {
     static let apiKey: String = {
         guard let key = Bundle.main.object(forInfoDictionaryKey: "APIKey") as? String else {
-            fatalError("API Key is missing in Config.plist")
+            fatalError("API Key is missing in info.plist")
         }
         return key
     }()
     
     static let baseURL: String = {
         guard let url = Bundle.main.object(forInfoDictionaryKey: "BaseURL") as? String else {
-            fatalError("Base URL is missing in Config.plist")
+            fatalError("Base URL is missing in info.plist")
+        }
+        return url
+    }()
+    
+    static let imageBaseURL: String = {
+        guard let url = Bundle.main.object(forInfoDictionaryKey: "ImageBaseUrl") as? String else {
+            fatalError("Image Base URL is missing in info.plist")
         }
         return url
     }()
